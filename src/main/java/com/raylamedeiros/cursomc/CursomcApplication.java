31f73ebe5908e6ae6314193852f9d049afe2c971
Rayla Medeiros <rayla.medeiiros@gmail.com>
2018-08-12 12:25:08 -0300

    Projeto criado

---
+++ src/main/java/com/raylamedeiros/cursomc/CursomcApplication.java
@@ -0,0 +1,12 @@
+package com.raylamedeiros.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+}
