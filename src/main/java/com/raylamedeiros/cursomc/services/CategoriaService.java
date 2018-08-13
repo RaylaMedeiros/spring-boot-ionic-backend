e66c2764fe18e35ea9c5296273852b8518a00b51
Rayla Medeiros <rayla.medeiiros@gmail.com>
2018-08-13 14:41:16 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/raylamedeiros/cursomc/services/CategoriaService.java
+++ src/main/java/com/raylamedeiros/cursomc/services/CategoriaService.java
@@ -1,5 +1,7 @@
 package com.raylamedeiros.cursomc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -44,5 +46,9 @@ public class CategoriaService {
 		} catch(DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 }
