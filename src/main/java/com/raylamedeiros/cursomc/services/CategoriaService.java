cf6e37859a9d79efdb9f27e7cc6986801a0d9c9a
Rayla Medeiros <rayla.medeiiros@gmail.com>
2018-08-13 15:54:56 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/raylamedeiros/cursomc/services/CategoriaService.java
+++ src/main/java/com/raylamedeiros/cursomc/services/CategoriaService.java
@@ -10,6 +10,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.raylamedeiros.cursomc.domain.Categoria;
+import com.raylamedeiros.cursomc.dto.CategoriaDTO;
 import com.raylamedeiros.cursomc.repositories.CategoriaRepository;
 import com.raylamedeiros.cursomc.services.exceptions.DataIntegrityException;
 import com.raylamedeiros.cursomc.services.exceptions.ObjectNotFoundException;
@@ -58,5 +59,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
