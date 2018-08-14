ded2705833a936bb816644e22fa9143180d4345c
Rayla Medeiros <rayla.medeiiros@gmail.com>
2018-08-13 22:25:41 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/raylamedeiros/cursomc/services/PedidoService.java
+++ src/main/java/com/raylamedeiros/cursomc/services/PedidoService.java
@@ -22,4 +22,8 @@ public class PedidoService {
 
 		return obj;
 	}
+	
+	/*public Pedido insert(Pedido obj) {
+		
+	}*/
 }
---
+++ src/main/java/com/raylamedeiros/cursomc/services/ProdutoService.java
@@ -0,0 +1,36 @@
+package com.raylamedeiros.cursomc.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import com.raylamedeiros.cursomc.domain.Categoria;
+import com.raylamedeiros.cursomc.domain.Produto;
+import com.raylamedeiros.cursomc.repositories.CategoriaRepository;
+import com.raylamedeiros.cursomc.repositories.ProdutoRepository;
+import com.raylamedeiros.cursomc.services.exceptions.ObjectNotFoundException;
+@Service
+public class ProdutoService {
+	
+	@Autowired
+	private ProdutoRepository repo;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	public Produto find(Integer id) {
+		Produto obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Produto.class.getName());
+		}
+		return obj;
+	}
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAll(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+}
